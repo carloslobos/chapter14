41d88f6cbf21ee4c9034429b581aed80d33bd137
Karol Lobos <karollobos@interia.pl>
2019-12-23 20:50:56 +0100

    Try to make HASH and BAR work good

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     // options
     cellAlign: 'left',
-    contain: true
+    contain: true,
+    pageDots: false
 });
 
 // element argument can be a selector string
@@ -11,12 +12,22 @@ var flkty = new Flickity('.main-carousel', {
     // options
 });
 
+// 10
 var flkty = new Flickity('.carousel', {
     hash: true,
 });
 
-flkty.reloadCells();
+// 11
+var buttonReset = document.querySelector('.button-reset');
+var firstSlide = document.querySelector('#carousel-cell1');
 
+buttonReset.addEventListener('click', function(event) {
+
+    //var index = buttonReset.indexOf(event.target);
+    flkty.select(firstSlide);
+});
+
+//12
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on('scroll', function(progress) {
