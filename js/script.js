f80bd606794388f89ec0237193ef5cf5e968be97
Karol Lobos <karollobos@interia.pl>
2020-01-04 22:57:52 +0100

    First attempt of connecting

--- js/script.js
+++ js/script.js
@@ -47,24 +47,22 @@ flkty.on('scroll', function(progress) {
 
 initMap = function() {
     var losAngelesLoc = { lat: 34.054302, lng: -118.246977 };
-    // var dubajLoc = { lat: 25.219288, lng: 55.277142 };
-    // var kubaLoc = { lat: 21.612649, lng: -78.972890 };
-    // var malediwyLoc = { lat: -0.609701, lng: 73.111158 };
-    // var rzymLoc = { lat: 41.903343, lng: 12.494790 };
 
-    // The map, centered at Uluru
+
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 6, center: losAngelesLoc });
-    // The marker, positioned at Uluru
-    //var markerOne = new google.maps.Marker({ position: losAngelesLoc, map: map });
-    // var markerTwo = new google.maps.Marker({ position: dubajLoc, map: map });
-    // var markerThree = new google.maps.Marker({ position: kubaLoc, map: map });
-    // var markerFour = new google.maps.Marker({ position: malediwyLoc, map: map });
-    // var markerFive = new google.maps.Marker({ position: rzymLoc, map: map });
 
     for (var i = 0; i < slides.length; i++) {
         var element = slides[i];
 
         var marker = new google.maps.Marker({ position: element.cords, map: map });
+
+        marker.addListener('click', function(event) {
+            flkty.select(i);
+        });
+
+        flkty.on('change', function(i) {
+            map = { zoom: 6, center: element.cords }
+        });
     }
 };
